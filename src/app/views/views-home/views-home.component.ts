80143c668ab449f78086cd6ca94dbf0375c764a6
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2022-11-30 00:31:30 +0100

    statistics and item list components added

--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-views-home',
   templateUrl: './views-home.component.html',
-  styleUrls: ['./views-home.component.scss']
+  styleUrls: ['./views-home.component.scss'],
 })
 export class ViewsHomeComponent {
-
+  stats = [
+    { value: 20, label: 'number of users' },
+    { value: 900, label: 'revenue' },
+    { value: 50, label: 'reviews' },
+  ];
+  items = [
+    {
+      image: '/assets/images/couch.jpeg',
+      title: 'Couch',
+      description: 'This is a great couch to sit on',
+    },
+    {
+      image: '/assets/images/dresser.jpeg',
+      title: 'Dresser',
+      description: 'This is a neat dresser',
+    },
+  ];
 }
